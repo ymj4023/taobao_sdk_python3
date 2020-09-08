5a873126adac2c45ad4005e2f17b2956a53b71dc
ymj <ymj123465@gmail.com>
2020-09-08 17:01:45 +0800

    Revert "no message"
    This reverts commit 82981e8470c6084561928a0953d53a4c4f148366.

--- top/api/rest/TbkActivityInfoGetRequest.py
+++ top/api/rest/TbkActivityInfoGetRequest.py
@@ -11,7 +11,7 @@ class TbkActivityInfoGetRequest(RestApi):
         # 代理id
 		self.relation_id = None
         # 官方活动物料id，由官方运营提供或从官方平台上获取
-        self.activity_material_id = None
+        self.activity_material_id
         # 自定义输入串，英文和数字组成，长度不能大于12个字符，区分不同的推广渠道
         self.union_id = None
 
